ddba5ba1415c02f5f0669dc98698cf66a112209a
Vikrant Negi <vikrant.negi@naukri.com>
2023-03-28 02:37:36 +0530

    feat: add splash screen

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -1,15 +1,43 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {StackScreenProps} from '@react-navigation/stack';
+import React, {useEffect} from 'react';
+import {View, Image, StyleSheet, Dimensions} from 'react-native';
+import {ScreenParamList} from '../../Navigation/AppNavigator';
 
-const SplashScreen = () => {
+const windowWidth = Dimensions.get('window').width;
+
+const SplashScreen = ({
+  navigation,
+}: StackScreenProps<ScreenParamList, 'Splash'>) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Home');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>SplashScreens</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/splash-screen.jpg')}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d53f27',
+  },
+  logo: {
+    width: windowWidth,
+    height: windowWidth * 1.25,
+  },
+});
 
 export default SplashScreen;
