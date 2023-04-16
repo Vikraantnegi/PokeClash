236e6d95ec63f346e5e5f89e776ad2f1d636c2db
Vikraantnegi <vikrant.negi74@gmail.com>
2023-04-16 05:36:04 +0530

    feat: firebase setup

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2,3 +2,11 @@ export interface HashMap {
   [key: number]: any;
   [key: string]: any;
 }
+
+export interface FirebaseUserObject {
+  username: string;
+  email: string | null | undefined;
+  password: string;
+  userId: string | undefined;
+  isVerified: boolean | undefined;
+}
